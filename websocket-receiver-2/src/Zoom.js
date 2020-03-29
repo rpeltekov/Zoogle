b414353a5decfc6317c4f03db1f01aac8408c562
Robert Peltekov <robert.peltekov@berkeley.edu>
2020-03-29 00:33:18 -0700

    working login to meeting

--- websocket-receiver-2/src/Zoom.js
+++ websocket-receiver-2/src/Zoom.js
@@ -1,83 +1,86 @@
 import React, { Component } from 'react'
-
 import { ZoomMtg } from "@zoomus/websdk";
-
 const API_KEY = 'n1TaQgWDRfKlseHZEakN8w';
 const API_SECRET = 'DLnpEoxBB1dAJVXrbEJaWKjoCSEjwTYiJoSr';
-
-const meetConfig = {
-    apiKey: API_KEY,
-    apiSecret: API_SECRET,
-    meetingNumber: 3319712923,
-    userName: 'Google Assistant',
-    userEmail: 'rpeletkov@gmail.com',
-    passWord: '',
-    leaveUrl: 'https://zoom.us',
-    role: 0
-};
-
 export default class Zoom extends Component {
+    constructor(props) {
+        super(props);
+        this.state ={
+            name: this.props.name,
+            url: this.props.url
+        }
+    }
     state = {
         meetingLaunched: false,
     }
-
-    launchMeeting = () => {
-        ZoomMtg.generateSignature({
-            meetingNumber: meetConfig.meetingNumber,
-            apiKey: meetConfig.apiKey,
-            apiSecret: meetConfig.apiSecret,
-            role: meetConfig.role,
-            success(res) {
-                console.log('signature', res.result);
-
-                ZoomMtg.init({
-                    leaveUrl: 'http://www.zoom.us',
-                    success() {
-                        ZoomMtg.join(
-                            {
-                                meetingNumber: meetConfig.meetingNumber,
-                                userName: meetConfig.userName,
-                                signature: res.result,
-                                apiKey: meetConfig.apiKey,
-                                userEmail: 'email@gmail.com',
-                                passWord: meetConfig.passWord,
-                                success() {
-                                    //$('#nav-tool').hide();
-                                    console.log('join meeting success');
-                                    setTimeout(function () {
-                                        var startButton = document.getElementById('pc-join');
-                                        startButton.click();
-                                    }, 3000);
-                                },
-                                error(res) {
-                                    console.log(res);
+    launchMeeting = (url, name) => {
+        console.log(url, name, 'launch meeting');
+        if (url != '' && name != '') {
+            console.log(url, name, 'passed empty if statement');
+            const meetConfig = {
+                apiKey: API_KEY,
+                apiSecret: API_SECRET,
+                meetingNumber: parseInt(url, 10),
+                userName: name,
+                userEmail: 'rpeltekov@gmail.com',
+                passWord: '',
+                leaveUrl: 'https://zoom.us',
+                role: 0
+            };
+            ZoomMtg.generateSignature({
+                meetingNumber: meetConfig.meetingNumber,
+                apiKey: meetConfig.apiKey,
+                apiSecret: meetConfig.apiSecret,
+                role: meetConfig.role,
+                success(res) {
+                    ZoomMtg.init({
+                        leaveUrl: 'http://www.zoom.us',
+                        success() {
+                            ZoomMtg.join(
+                                {
+                                    meetingNumber: meetConfig.meetingNumber,
+                                    userName: meetConfig.userName,
+                                    signature: res.result,
+                                    apiKey: meetConfig.apiKey,
+                                    userEmail: 'email@gmail.com',
+                                    passWord: meetConfig.passWord,
+                                    success() {
+                                        setTimeout(function () {
+                                            var startButton = document.getElementById('pc-join');
+                                            startButton.click();
+                                        }, 3000);
+                                    },
+                                    error(res) {
+                                        console.log(res, "1");
+                                    }
                                 }
-                            }
-                        );
-
-                    },
-                    error(res) {
-                        console.log(res);
-                    }
-                });
-
-            }
-        });
+                            );
+                        },
+                        error(res) {
+                            console.log(res, "2");
+                        }
+                    });
+                }
+            });
+        }
     }
-
     componentDidMount() {
-        ZoomMtg.setZoomJSLib("https://source.zoom.us/1.7.0/lib", "/av");
-        ZoomMtg.preLoadWasm();
-        ZoomMtg.prepareJssdk();
-    }
 
+    }
+    shouldComponentUpdate(nextProps) {
+        return nextProps.name != this.props.name
+            && nextProps.url != this.props.url;
+    }
     render() {
-        const { meetingLaunched} = this.state;
+        const { meetingLaunched } = this.state;
+        if (this.props.url != '' && this.props.name != '') {
+            this.launchMeeting(this.props.url, this.props.name);
+        }
         // Displays a button to launch the meeting when the meetingLaunched state is false
         return (
             <>
                 {!meetingLaunched ?
-                    <button className="launchButton" onClick={this.launchMeeting}>Launch Meeting</button>
+                    <button className="launchButton" onClick={() => this.launchMeeting(this.props.url, this.props.name)}>Launch Meeting</button>
                     :
                     <></>
                 }
--- websocket-receiver-2/src/setting.js
+++ websocket-receiver-2/src/setting.js
@@ -1,58 +1,14 @@
 import React, {Component} from "react";
 import "./setting.css";
-import firebase from "./firebase.js";
 
 class Setting extends Component {
-    constructor() {
-        super();
+    constructor(props) {
+        super(props);
         this.state = {
             currentInit: [],
             settings: [],
             isMuted: false
         };
-    }
-
-    componentDidMount() {
-        this.receiveInit();
-        this.receiveSettings();
-    }
-
-    receiveSettings() {
-        const usernameRef = firebase.database().ref("settings");
-        usernameRef.on("value", snapshot => {
-            let commands = snapshot.val();
-            let tempState = [];
-            for (let word in commands) {
-                if (commands[word].command === "mute") {
-                    this.setState({isMuted: true});
-                    console.log("isMute: " + this.state.isMuted);
-                } else if (commands[word].command === "unmute") {
-                    this.setState({isMuted: false});
-                    console.log("isMute: " + this.state.isMuted);
-                }
-                tempState.push({command: commands[word].command});
-            }
-            this.setState({
-                settings: tempState
-            });
-        });
-    }
-
-    receiveInit() {
-        const usernameRef = firebase.database().ref("items");
-        usernameRef.on("value", snapshot => {
-            let items = snapshot.val();
-            let tempState = [];
-            for (let word in items) {
-                tempState.push({
-                    url: items[word].url,
-                    user: items[word].user
-                });
-            }
-            this.setState({
-                currentInit: tempState
-            });
-        });
     }
 
     render() {
